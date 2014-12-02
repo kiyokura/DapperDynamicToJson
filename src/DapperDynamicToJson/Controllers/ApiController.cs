1f32e7e6a442dd9bcc0f53a6105907ea7f620e30
Kiyokura Narami <kiyokura@gmail.com>
2014-12-02 23:00:38 +0900

    first commit

---
+++ src/DapperDynamicToJson/Controllers/ApiController.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Web.Mvc;
+using Dapper;
+
+namespace DapperDynamicToJson.Controllers
+{
+  public class ApiController : Controller
+  {
+    public JsonResult Default()
+    {
+      var json = new JsonResult()
+      {
+        Data = GetDynamicRecords(),
+        JsonRequestBehavior = JsonRequestBehavior.AllowGet
+      };
+      return json;
+    }
+
+    public JsonResult JsonNet()
+    {
+      var json = new JsonNetResult()
+        {
+          Data = GetDynamicRecords(),
+          JsonRequestBehavior = JsonRequestBehavior.AllowGet
+        };
+      return json;
+    }
+
+    private IList<dynamic> GetDynamicRecords()
+    {
+      var conStr = ConfigurationManager.ConnectionStrings["Default"].ConnectionString;
+      using (var con = new System.Data.SqlClient.SqlConnection(conStr))
+      {
+        con.Open();
+        var sql = @"select 'Taro' as name, 20 as age union select 'Jiro' as name, 25 as age;";
+        var records = con.Query<dynamic>(sql);
+        return records.ToList();
+      }
+    }
+
+  }
+
+}
---
+++ src/DapperDynamicToJson/Controllers/JsonNetResult.cs
@@ -0,0 +1,45 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Web;
+using System.Web.Mvc;
+
+namespace DapperDynamicToJson.Controllers
+{
+
+  // 実装は ASP.NET MVC 5.xのJsonResultをほぼ踏襲。 http://aspnetwebstack.codeplex.com/
+public class JsonNetResult : JsonResult
+{
+  public override void ExecuteResult(ControllerContext context)
+  {
+    if (context == null)
+    {
+      throw new ArgumentNullException("context");
+    }
+    if (JsonRequestBehavior == JsonRequestBehavior.DenyGet &&
+        String.Equals(context.HttpContext.Request.HttpMethod, "GET", StringComparison.OrdinalIgnoreCase))
+    {
+      throw new InvalidOperationException("JSON GET is not allowed");
+    }
+
+    HttpResponseBase response = context.HttpContext.Response;
+
+    if (!String.IsNullOrEmpty(ContentType))
+    {
+      response.ContentType = ContentType;
+    }
+    else
+    {
+      response.ContentType = "application/json";
+    }
+    if (ContentEncoding != null)
+    {
+      response.ContentEncoding = ContentEncoding;
+    }
+    if (Data != null)
+    {
+      response.Write(JsonConvert.SerializeObject(this.Data));
+    }
+  }
+}
+
+}
